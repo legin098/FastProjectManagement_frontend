50580373b380cbdfe488df22abb1f7fed98be87d
john fredy mosquera <jhon.mosquera@campusvirtual.aunarvillavicencio.edu.co>
2022-05-04 20:45:48 -0500

    Formulario de registar

--- src/paginas/OlvidePassword.jsx
+++ src/paginas/OlvidePassword.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom"
 
 const OlvidePassword = () => {
   return (
-    <div>OlvidePassword</div>
+    <div>
+      
+    </div>
   )
 }
 
--- src/paginas/Registrar.jsx
+++ src/paginas/Registrar.jsx
@@ -1,9 +1,95 @@
-
+import { Link } from "react-router-dom";
 
 const Registrar = () => {
   return (
-    <div>Registrar</div>
-  )
-}
+    <>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">
+        Crea tu cuenta y administra tus{" "}
+        <span className="text-slate-700">proyectos</span>
+      </h1>
 
-export default Registrar
+      <form className="my-10 bg-white shadow rounded-lg p-10">
+        <div className="my-5">
+          <label
+            className="uppercase text-gray-600 block tex-xl font-bold"
+            htmlFor="nombre"
+          >
+            Nombre
+          </label>
+          <input
+            id="nombre"
+            type="text"
+            placeholder="Tu Nombre"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+        <div className="my-5">
+          <label
+            className="uppercase text-gray-600 block tex-xl font-bold"
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <input
+            id="email"
+            type="email"
+            placeholder="Email de registro"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+        <div className="my-5">
+          <label
+            className="uppercase text-gray-600 block tex-xl font-bold"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            id="password"
+            type="password"
+            placeholder="Password de registro"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+        <div className="my-5">
+          <label
+            className="uppercase text-gray-600 block tex-xl font-bold"
+            htmlFor="password2"
+          >
+            Repetir Password
+          </label>
+          <input
+            id="password2"
+            type="password"
+            placeholder="Repetir tu Password"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+
+        <input
+          type="submit"
+          value="Crear Cuenta"
+          className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hove:bg-sky-800 transition-colors"
+        />
+      </form>
+
+      <nav className="lg:flex lg:justify-between">
+        <Link
+          className="block text-center my-5 text-slate-500 uppercase text-sm"
+          to="/"
+        >
+          ¿Ya tienes una cuenta? Inicia Sesión
+        </Link>
+
+        <Link
+          className="block text-center my-5 text-slate-500 uppercase text-sm"
+          to="/olvide-password"
+        >
+          Olvide mi password
+        </Link>
+      </nav>
+    </>
+  );
+};
+
+export default Registrar;
