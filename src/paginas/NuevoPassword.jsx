8023ee12a902290d1904c0f25d438c865ea328ee
jesus cruz <jesus.cruz@campusvirtual.aunarvillavicencio.edu.co>
2022-05-09 13:30:13 -0500

    funcionalidad restablecer password

--- src/paginas/NuevoPassword.jsx
+++ src/paginas/NuevoPassword.jsx
@@ -1,33 +1,85 @@
+import { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import axios from "axios";
+import Alerta from "../components/Alerta";
+
 const NuevoPassword = () => {
+  const [password, setPassword] = useState("");
+  const [tokenValido, setTokenValido] = useState(false);
+  const [alerta, setAlerta] = useState({});
+  const [passwordModificado, setPasswordModificado] = useState(false);
+
+  const params = useParams();
+  const { token } = params;
+
+  useEffect(() => {
+    const comprobarToken = async () => {
+      try {
+        await axios.get(`http://localhost:4000/api/usuarios/restablecer-password/${token}`);
+        setTokenValido(true);
+      } catch (e) {
+        setAlerta({ msg: e.response.data.msg, error: true });
+      }
+    };
+    comprobarToken();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (password.length < 6) {
+      setAlerta({ msg: "El password debe tener al menos 6 caracteres", error: true });
+      return;
+    }
+
+    try {
+      const url = `http://localhost:4000/api/usuarios/restablecer-password/${token}`;
+      const { data } = await axios.post(url, { password });
+      setAlerta({ msg: data.msg, error: false });
+      setPasswordModificado(true);
+    } catch (e) {
+      setAlerta({ msg: e.response.data.msg, error: true });
+    }
+  };
+
+  const { msg } = alerta;
+
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl capitalize">
-        Reestablece tu password y no pierdas acceso a tus{" "}
-        <span className="text-slate-700">proyectos</span>
+        Restablece tu password y no pierdas acceso a tus <span className="text-slate-700">proyectos</span>
       </h1>
 
-      <form className="my-10 bg-white shadow rounded-lg p-10">
-        <div className="my-5">
-          <label
-            className="uppercase text-gray-600 block tex-xl font-bold"
-            htmlFor="password"
-          >
-           Nuevo Password
-          </label>
-          <input
-            id="password"
-            type="password"
-            placeholder="Escribe tu Nuevo Password"
-            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
-          />
-        </div>
+      {msg && <Alerta alerta={alerta} />}
 
-        <input
-          type="submit"
-          value="Guardar Nuevo Password"
-          className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hove:bg-sky-800 transition-colors"
-        />
-      </form>
+      {tokenValido && (
+        <form className="my-10 bg-white shadow rounded-lg p-10" onSubmit={handleSubmit}>
+          <div className="my-5">
+            <label className="uppercase text-gray-600 block tex-xl font-bold" htmlFor="password">
+              Nuevo Password
+            </label>
+            <input
+              id="password"
+              type="password"
+              placeholder="Escribe tu Nuevo Password"
+              className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <input
+            type="submit"
+            value="Guardar Nuevo Password"
+            className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hove:bg-sky-800 transition-colors"
+          />
+        </form>
+      )}
+      {passwordModificado && (
+        <Link className="block text-center my-5 text-slate-500 uppercase text-sm" to="/">
+          Inicia Sesión
+        </Link>
+      )}
     </>
   );
 };
--- src/paginas/OlvidePassword.jsx
+++ src/paginas/OlvidePassword.jsx
@@ -1,20 +1,42 @@
-import { Link } from "react-router-dom"
+import axios from "axios";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import Alerta from "../components/Alerta";
 
 const OlvidePassword = () => {
+  const [email, setEmail] = useState("");
+  const [alerta, setAlerta] = useState({});
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (email === "" || email.length < 6) {
+      setAlerta({ msg: "El email es obligatorio", error: true });
+      return;
+    }
+
+    try {
+      const { data } = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/usuarios/restablecer-password`, {
+        email,
+      });
+      setAlerta({ msg: data.msg, error: false });
+    } catch (e) {
+      setAlerta({ msg: e.response.data.msg, error: true });
+    }
+  };
+
+  const { msg } = alerta;
+
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl capitalize">
-        Recuperar tu acceso y no pierdad tus{" "}
-        <span className="text-slate-700">proyectos</span>
+        Recupera tu acceso y no pierdas tus <span className="text-slate-700">proyectos</span>
       </h1>
 
-      <form className="my-10 bg-white shadow rounded-lg p-10">
-      
+      {msg && <Alerta alerta={alerta} />}
+
+      <form className="my-10 bg-white shadow rounded-lg p-10" onSubmit={handleSubmit}>
         <div className="my-5">
-          <label
-            className="uppercase text-gray-600 block tex-xl font-bold"
-            htmlFor="email"
-          >
+          <label className="uppercase text-gray-600 block tex-xl font-bold" htmlFor="email">
             Email
           </label>
           <input
@@ -22,8 +44,10 @@ const OlvidePassword = () => {
             type="email"
             placeholder="Email de Registro"
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
-        </div>  
+        </div>
 
         <input
           type="submit"
@@ -33,20 +57,16 @@ const OlvidePassword = () => {
       </form>
 
       <nav className="lg:flex lg:justify-between">
-        <Link
-          className="block text-center my-5 text-slate-500 uppercase text-sm"
-          to="/"
-        >
+        <Link className="block text-center my-5 text-slate-500 uppercase text-sm" to="/">
           ¿Ya tienes una cuenta? Inicia Sesión
         </Link>
 
-        <Link
-          className='block text-center my-5 text-slate-500 uppercase text-sm'
-            to="/registrar"
-          >¿No tienes una cuenta? Registrate</Link>
+        <Link className="block text-center my-5 text-slate-500 uppercase text-sm" to="/registrar">
+          ¿No tienes una cuenta? Registrate
+        </Link>
       </nav>
     </>
-  )
-}
+  );
+};
 
-export default OlvidePassword
+export default OlvidePassword;
